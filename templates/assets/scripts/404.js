b4c90f586ca5e4c12020e810f67f6d3ca5b05d47
pmaxime <maxime.poiret@ynov.com>
2023-06-20 14:12:53 +0200

    update: "404.css + 404.html + 404.js video1.mp4"

--- templates/assets/scripts/404.js
+++ templates/assets/scripts/404.js
@@ -1,19 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var audio = document.getElementById('myAudio');
-    var video1 = document.getElementById('myVideo1');
-    var video2 = document.getElementById('myVideo2');
-    var video3 = document.getElementById('myVideo3');
-    var video4 = document.getElementById('myVideo4');
-    var video5 = document.getElementById('myVideo5');
-
-    audio.volume = 0.2;
-    video2.volume = 0.2;
-    video3.volume = 0.2;
-
-    audio.play();
-    video1.play();
-    video2.play();
-    video3.play();
-    video4.play();
-    video5.play();
+window.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('myVideo');
+    video.play();
 });
