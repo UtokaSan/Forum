7a82a8141f798d27791bee2649c15a8424b65a2b
Nathan Reungoat <reungoat.nathan@gmail.com>
2023-06-21 13:45:22 +0200

    update

--- templates/assets/scripts/MainPage.js
+++ templates/assets/scripts/MainPage.js
@@ -1,25 +1,20 @@
 function afficherContenu(evt, contenuId) {
-    // Cacher tous les contenus des onglets
     var contenus = document.getElementsByClassName("contenu-onglet");
     for (var i = 0; i < contenus.length; i++) {
         contenus[i].style.display = "none";
     }
 
-    // Supprimer la classe "active" de tous les onglets
     var onglets = document.getElementsByClassName("onglet");
     for (var i = 0; i < onglets.length; i++) {
         onglets[i].className = onglets[i].className.replace(" active", "");
     }
 
-    // Afficher le contenu de l'onglet sélectionné
     document.getElementById(contenuId).style.display = "block";
 
-    // Ajouter la classe "active" à l'onglet sélectionné
     evt.currentTarget.className += " active";
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    // Activer la catégorie "Chat Général" et afficher son contenu
     var defaultCategoryButton = document.querySelector(".onglet[data-category='contenu1']");
     defaultCategoryButton.classList.add("active");
     var defaultCategoryContent = document.querySelector(".contenu-onglet[data-content='contenu1']");
@@ -52,6 +47,7 @@ fetch("/api/display-post", {
 })
     .then(response => response.json())
     .then(data => {
+        console.log(data)
         data.forEach(post => {
             var newDiv = document.createElement("div");
             newDiv.id = "post-" + post.id;
@@ -74,6 +70,7 @@ fetch("/api/display-post", {
             if (categoryDiv) {
                 categoryDiv.appendChild(newDiv);
             }
+
         });
     })
     .catch(error => {
