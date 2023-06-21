ba3aa3a5559c7091c7f22a5014f1d94916f45971
pmaxime <maxime.poiret@ynov.com>
2023-06-22 00:04:50 +0200

    update: "postPage.js"

--- templates/assets/scripts/postPage.js
+++ templates/assets/scripts/postPage.js
@@ -1,5 +1,3 @@
-
-
 var url = new URL(window.location.href);
 var param1Value = url.searchParams.get('id');
 
@@ -18,14 +16,52 @@ fetch("/api/takepostid", {
     .then(data => {
         console.log(data.info[0].title);
         var title = document.querySelector('.Title');
-        var texttitle = data.info[0].title;
-        title.innerText = texttitle;
+        title.innerText = data.info[0].title;
 
         var description= document.querySelector('.contentPost');
-        var textdescription = data.info[0].text;
-        description.innerText = textdescription;
+        description.innerText = data.info[0].text;
 
+        var like= document.querySelector('.text-like');
+        like.innerText = data.info[0].like;
+
+        var dislike= document.querySelector('.text-dislike');
+        dislike.innerText = data.info[0].dislike;
     })
     .catch(error => {
         console.error("Error sending data id", error);
     });
+
+const likeButton = document.querySelector('.like');
+const dislikeButton = document.querySelector('.dislike');
+
+likeButton.addEventListener('click', envoyerLike);
+dislikeButton.addEventListener('click', envoyerDislike);
+
+function envoyerLike() {
+    sendData('like');
+}
+
+function envoyerDislike() {
+    sendData('dislike');
+}
+
+function sendData (formDataSend) {
+    const data = {
+        reactions: formDataSend,
+        post_id: parseInt(param1Value),
+    };
+    fetch("/api/likeordislike", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: data,
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
