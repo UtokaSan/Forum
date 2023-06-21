46873d2a7f2ce2fd9717085e4bf57dfdf9e0b35d
pmaxime <maxime.poiret@ynov.com>
2023-06-21 21:22:31 +0200

    update: "post.js + postPage.html PostPage.js"

--- templates/assets/scripts/PostPage.js
+++ templates/assets/scripts/PostPage.js
@@ -19,3 +19,24 @@ btn2.addEventListener('click', function() {
 
 });
 
+var url = new URL(window.location.href);
+var param1Value = url.searchParams.get('id');
+
+var parameterUrl = {
+    id: parseInt(param1Value)
+};
+
+fetch("/api/takepostid", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(parameterUrl),
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error("Error sending data id", error);
+    });
