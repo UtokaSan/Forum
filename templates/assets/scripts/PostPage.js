8e221ef6f572b8f9ae02cecf253e97a6444652d3
pmaxime <maxime.poiret@ynov.com>
2023-06-21 21:49:28 +0200

    update: "404.html + PostPage.js"

--- templates/assets/scripts/PostPage.js
+++ templates/assets/scripts/PostPage.js
@@ -35,7 +35,15 @@ fetch("/api/takepostid", {
 })
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        console.log(data.info[0].title);
+        var title = document.querySelector('.Title');
+        var texttitle = data.info[0].title;
+        title.innerText = texttitle;
+
+        var description= document.querySelector('.contentPost');
+        var textdescription = data.info[0].text;
+        description.innerText = textdescription;
+
     })
     .catch(error => {
         console.error("Error sending data id", error);
